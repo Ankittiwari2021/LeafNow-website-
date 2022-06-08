b8fc11d1dbca3638dc3460a513826aa312503b37
Ankittiwari2021 <86077524+Ankittiwari2021@users.noreply.github.com>
2022-06-08 19:31:16 +0530

    Add files via upload

---
+++ leafnow/database/dummy_data.sql
@@ -0,0 +1,14 @@
+INSERT INTO 
+  `cities` 
+    (`id`, `name`) 
+  VALUES
+    (1, 'Bengaluru');
+
+INSERT INTO 
+  `users` 
+    (`id`, `email`, `password`, `full_name`, `phone`, `gender`, `college_name`) 
+  VALUES
+    (1, 'anuj.kalbalia@gmail.com', 'b1b3773a05c0ed0176787a4f1574ff0075f7521e', 'Anuj Kalbalia', '9535100112', 'male', 'NITD'),
+    (2, 'shadab@gmail.com', 'b1b3773a05c0ed0176787a4f1574ff0075f7521e', 'Shadab Alam', '9876543210', 'male', 'NITJ'),
+    (3, 'aditya@gmail.com', 'b1b3773a05c0ed0176787a4f1574ff0075f7521e', 'Aditya Sood', '9876543210', 'male', 'Chandigarh University'),
+    (4, 'radhika@gmail.com', 'b1b3773a05c0ed0176787a4f1574ff0075f7521e', 'Radhika Bhatia', '9876543210', 'female', 'Delhi University');
